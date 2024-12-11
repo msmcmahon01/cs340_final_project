72d64fa4384aec6681b615b98ac791b85ef7de89
Matthew McMahon <msmcmahon01@gmail.com>
2024-12-10 22:53:50 -0800

    a

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	//$currentpage="View Employees"; 
+	//$currentpage="View Employees";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,9 +28,9 @@
     </style>
     <script type="text/javascript">
         $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
+            $('[data-toggle="tooltip"]').tooltip();
         });
-		
+
     </script>
 </head>
 <body>
@@ -52,7 +52,7 @@
             <div class="row">
                 <div class="col-md-12">
 		    <div>
-		        
+
             </div>
                 <?php
                 // Include config file
@@ -62,10 +62,10 @@
                 if (isset($_POST['deleteRoutes'])) {
                     $deleteDate = $_POST['deleteDate'];
                     $deleteGym = $_POST['deleteGym'];
-                
+
                     // Debugging: Print out the values being passed
                     echo "<p>Deleting routes on date: $deleteDate for Gym ID: $deleteGym</p>";
-                
+
                     // Check for matching records before deletion
                     $checkSql = "SELECT RouteID FROM Route WHERE Created = '$deleteDate' AND GymID = $deleteGym;";
                     if ($checkResult = mysqli_query($link, $checkSql)) {
@@ -74,10 +74,10 @@
                             while ($row = mysqli_fetch_array($checkResult)) {
                                 echo "<p>Route ID: " . $row['RouteID'] . "</p>";
                             }
-                
+
                             // Call the stored procedure to delete routes
                             $sql = "CALL DeleteRoutesByDate('$deleteDate', $deleteGym);";
-                
+
                             if (mysqli_query($link, $sql)) {
                                 echo "<p>Routes created on $deleteDate at Gym $deleteGym have been deleted.</p>";
                             } else {
@@ -106,7 +106,7 @@
                 if ($selectedgym == "" && $selectedsetter ==""){
                     # No selections
                     echo "<h2>Routes</h2>";
-                    
+
                     $sql = "SELECT Route.RouteID,Setter.Name,Created,Difficulty,Color, OffColor, GymID, COALESCE(SUM(RouteHold.Amount),0) AS Total
                             FROM Route
                             NATURAL JOIN RouteHold
@@ -118,7 +118,7 @@
                                         5 => '#c6dd74', 6 => '#cad361', 7 => '#cec94f', 8=>'#d4be3e', 9=> '#d9b22d',
                                     10 => '#dfa51c', 11 => '#e5980d', 12 => '#eb8902', 13 => '#f7660d', 14 => '#fb5019',
                                 15 => '#fc633c', 16=>'#ff5043');
-                    
+
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
@@ -141,11 +141,11 @@
                                         echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['GymID'] . "</td>";
                                         echo "<td bgcolor='" . $difficulties[$row['Difficulty']] . "'>" . $row['Difficulty'] . "</td>";
                                         echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Name'] . "</td>";
-                                        echo "<td bgcolor='" . $row['Color'] . "'>" . $row['Color'] . "</td>";									
+                                        echo "<td bgcolor='" . $row['Color'] . "'>" . $row['Color'] . "</td>";
                                         echo "<td bgcolor='" . ($row['OffColor'] ? $row['OffColor'] : 'transparent') . "'>" . $row['OffColor'] . "</td>";
-                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Total'] . "</td>";	
-                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Created'] . "</td>";	
-                                                                            
+                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Total'] . "</td>";
+                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Created'] . "</td>";
+
                                         //echo "<td>";
                                         //    echo "<a href='viewProjects.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Projects' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
                                         //    echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
@@ -154,7 +154,7 @@
                                         echo "</td>";
                                     echo "</tr>";
                                 }
-                                echo "</tbody>";                            
+                                echo "</tbody>";
                             echo "</table>";
                             // Free result set
                             mysqli_free_result($result);
@@ -181,7 +181,7 @@
                                         5 => '#c6dd74', 6 => '#cad361', 7 => '#cec94f', 8=>'#d4be3e', 9=> '#d9b22d',
                                     10 => '#dfa51c', 11 => '#e5980d', 12 => '#eb8902', 13 => '#f7660d', 14 => '#fb5019',
                                 15 => '#fc633c', 16=>'#ff5043');
-                    
+
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
@@ -204,11 +204,11 @@
                                         echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['GymID'] . "</td>";
                                         echo "<td bgcolor='" . $difficulties[$row['Difficulty']] . "'>" . $row['Difficulty'] . "</td>";
                                         echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Name'] . "</td>";
-                                        echo "<td bgcolor='" . $row['Color'] . "'>" . $row['Color'] . "</td>";									
+                                        echo "<td bgcolor='" . $row['Color'] . "'>" . $row['Color'] . "</td>";
                                         echo "<td bgcolor='" . ($row['OffColor'] ? $row['OffColor'] : 'transparent') . "'>" . $row['OffColor'] . "</td>";
-                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Total'] . "</td>";	
-                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Created'] . "</td>";	
-                                                                            
+                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Total'] . "</td>";
+                                        echo "<td bgcolor='" . ($row['GymID']%2 ? 'FFFFFF':'F1F1F1'). "'>" . $row['Created'] . "</td>";
+
                                         echo "<td>";
                                             $hsc = htmlspecialchars(basename($_SERVER['REQUEST_URI']));
                                             $query_string = http_build_query(array(
@@ -225,7 +225,7 @@
                                         echo "</td>";
                                     echo "</tr>";
                                 }
-                                echo "</tbody>";                            
+                                echo "</tbody>";
                             echo "</table>";
                             // Free result set
                             mysqli_free_result($result);
@@ -244,7 +244,7 @@
                     echo '<input type="hidden" name="deleteGym" value="' . $selectedgym . '">';
                     echo '<input type="submit" name="deleteRoutes" value="Delete Routes">';
                     echo '</form>';
-                    
+
                     // Close connection
                     mysqli_close($link);
                 } elseif ($selectedgym=='' && $selectedsetter!='') {
